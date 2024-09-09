3e3dfeb588fbf182fb997fb86f2c82022890f942
aio97 <154350326+aio97@users.noreply.github.com>
2024-09-10 01:40:07 +0900

    Revert "しおり作成機能"

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,5 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: %i[top]
   
-  def top
-    if logged_in?
-      redirect_to books_path
-    end
-  end
+  def top; end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_09_125119) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_06_175443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "books", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "image"
-    t.string "outline_title"
-    t.text "outline_body"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.boolean "is_public", default: false, null: false
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_books_on_user_id"
-  end
-
-  create_table "plans", force: :cascade do |t|
-    t.date "date"
-    t.string "title"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.string "icon"
-    t.string "location"
-    t.string "location_url"
-    t.text "detail"
-    t.text "memo"
-    t.decimal "cost"
-    t.boolean "cost_display_per"
-    t.boolean "cost_display_total"
-    t.bigint "book_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_plans_on_book_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -57,7 +24,4 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_09_125119) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
-  add_foreign_key "books", "users"
-  add_foreign_key "plans", "books"
 end
