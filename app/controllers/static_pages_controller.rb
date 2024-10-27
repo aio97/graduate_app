0cba36835f49dc1c169fe7786ffe8b6298984765
szkmrn97 <marino.suzuki97@gmail.com>
2024-10-27 22:59:16 +0900

    トップページにしおりを表示 / フッターを編集

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: %i[top]
 
   def top
+    @books = Book.where(is_public: true).order(created_at: :asc).limit(6)
+
     if logged_in?
       redirect_to books_path
     end
