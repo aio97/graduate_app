6a394dc18b65be227fc1eae6ea4bff1d97b66727
szkmrn97 <marino.suzuki97@gmail.com>
2024-11-18 23:21:04 +0900

    マップ表示を修正

--- app/javascript/google_map.js
+++ app/javascript/google_map.js
@@ -1,7 +1,27 @@
-var MyLatLng = new google.maps.LatLng(35.65871995583428, 139.7454221687028); //座標を指定
-var Options = {
-    zoom: 15, //地図の縮尺値を指定
-    center: MyLatLng, //地図の中心座標
-    mapTypeId: 'roadmap' //地図の種類を指定
-};
-var map = new google.maps.Map(document.getElementById('google_map'), Options); //埋め込むMAPのidを指定
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
