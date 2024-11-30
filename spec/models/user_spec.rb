9be5f9f32e3b386e9a81a6c0cbbe6f201406a691
aio97 <marino_suzuki@icloud.com>
2024-11-30 16:07:41 +0900

    Bookモデルのバリデーションテストを追加”

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "バリデーションチェック" do
-    it "emailとpasswordが存在する場合、ユーザー登録されるか" do
+    it "emailとpasswordが存在する場合、有効であること" do
       user = build(:user)
       expect(user).to be_valid
     end
 
-    it "email、passwordが存在しない場合、ユーザー登録されないか" do
+    it "email、passwordが存在しない場合、無効であること" do
       user = build(:user, email: "", password: "")
       user.valid?
       expect(user.errors[:email]).to include('を入力してください')
       expect(user.errors[:password]).to include('は8文字以上で入力してください')
     end
 
-    it "emailが重複した場合、ユーザー登録されないか" do
+    it "emailが重複した場合、無効であること" do
       user1 = create(:user)
       user2 = build(:user, email: user1.email)
       user2.valid?
