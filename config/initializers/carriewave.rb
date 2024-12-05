aa7aa9e30433a18cfe5036f46a9c3249159e5141
aio97 <marino_suzuki@icloud.com>
2024-12-05 16:08:52 +0900

    デプロイ時のエラー修正

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -17,15 +17,15 @@ CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.development? # 開発中もs3使う
     config.storage :fog
     # config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'watashi-no-shiori'
-    config.asset_host = 'https://watashi-no-shiori.s3.amazonaws.com'
+    config.fog_directory  = "watashi-no-shiori"
+    config.asset_host = "https://watashi-no-shiori.s3.amazonaws.com"
     # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
     config.fog_public = false # ←コレ
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: 'AWS_ACCESS_KEY_ID',
-      aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
-      region: 'ap-southeast-2',
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: "ap-southeast-2",
       # path_style: true
     }
     config.cache_dir = "#{Rails.root}/tmp/uploads"
