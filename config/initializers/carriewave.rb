6821a0459d62e1a58411f9860cf61bf840a16ebd
aio97 <marino_suzuki@icloud.com>
2024-12-08 17:03:01 +0900

    lintチェックの修正

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -30,7 +30,7 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required unless using use_iam_profile
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                        # required unless using use_iam_profile
     # use_iam_profile:       true,                         # optional, defaults to false
-    region:                "ap-southeast-2",                  # optional, defaults to 'us-east-1'
+    region:                "ap-southeast-2"                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
